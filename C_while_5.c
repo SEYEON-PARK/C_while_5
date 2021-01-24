11aec814d892154d6f89e53e3975aecb52f1e3f5
SEYEON_PARK <fourleaf123@naver.com>
2021-01-25 01:37:29 +0900

    C언어 while문 연습!(5)

---
+++ C_while_5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a, A, b = 1;
+
+	printf("양의 정수를 입력하시오. : ");
+	scanf_s("%d", &a);
+
+	A = a;
+
+	while (a != 0)
+	{
+			b *= a;
+			a--;
+	}
+
+	printf("%d!은 %d입니다.\n", A, b);
+
+	return 0;
+}
